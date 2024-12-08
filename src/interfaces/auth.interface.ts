90fd6b05548f4ee826b3fc76ec72636f0e098ee1
Kossy-Augusta <kossyudogu@gmail.com>
2024-12-08 20:45:12 +0100

    new method

--- src/interfaces/auth.interface.ts
+++ src/interfaces/auth.interface.ts
@@ -2,5 +2,5 @@ import { Request, Response } from "express";
 
 export default interface UserAuthInterface{
     create (req: Request, res: Response): Promise<Response>,
-    // login (req: Request, res: Response): Promise<Response>
+    login (req: Request, res: Response): Promise<Response>
 }
