5f1812820b36f75019f9d3bb7d7658ef32c1cdbb
Kossy-Augusta <kossyudogu@gmail.com>
2024-12-09 16:19:20 +0100

    added JWT verification middleware to the send message route

--- src/routes/message-routes.ts
+++ src/routes/message-routes.ts
@@ -1,11 +1,12 @@
 import express, { Request, Response } from "express";
 import MessageController from "../controllers/messagesController";
+import verifyJWT from "../middleware/verifyjwt";
 
 
 const router = express.Router();
 const messageController = new MessageController();
 
-router.post('/message/send', (req: Request, res: Response)=>{
+router.post('/message/send', verifyJWT, (req: Request, res: Response)=>{
     messageController.sendMessage(req, res);
 });
 
