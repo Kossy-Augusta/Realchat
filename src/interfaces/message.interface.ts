fff561810d8c680917ff2f575d7ca9175f4c95e5
Kossy-Augusta <kossyudogu@gmail.com>
2024-12-10 13:00:53 +0100

    added new method

--- src/interfaces/message.interface.ts
+++ src/interfaces/message.interface.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
 export default interface MessageManagementInterface{
-    sendMessage(req: Request, res: Response): Promise<Response>,
+    index(req: Request, res: Response): Promise<Response>,
+    store(req: Request, res: Response): Promise<Response>,
 }
