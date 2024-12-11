e44f85b01217307a3115aa24c2c92f2284d94e68
Kossy-Augusta <kossyudogu@gmail.com>
2024-12-11 11:53:20 +0100

    added logout method

--- src/controllers/userAuthController.ts
+++ src/controllers/userAuthController.ts
@@ -11,24 +11,27 @@ export default class UserAuth implements UserAuthInterface{
     async create(req: Request, res: Response): Promise<Response> {
         const {error, value} = creatUserSchema.validate(req.body, {abortEarly: false});
         if (error) return handleError(res, 400, error);
-
-        // check if user already exists
-        const existingUser = await prisma.user.findUnique({
-            where: {
-                email: value.email
-            },
-        });
-        if (existingUser) return handleError(res, 409, "User already exist");
-        const hashPassword =await  bcrypt.hash(value.password, 10);
-        const createNewUser = await prisma.user.create({
-            data: {
-                username: value.username,
-                email: value.email,
-                password: hashPassword
-            }
-        });
-        const data: object = createNewUser
-        return handleSuccess(res, 201, "New User crated succesfully", data)
+        try {
+            // check if user already exists
+            const existingUser = await prisma.user.findUnique({
+                where: {
+                    email: value.email
+                },
+            });
+            if (existingUser) return handleError(res, 409, "User already exist");
+            const hashPassword =await  bcrypt.hash(value.password, 10);
+            const createNewUser = await prisma.user.create({
+                data: {
+                    username: value.username,
+                    email: value.email,
+                    password: hashPassword
+                }
+            });
+            const data: object = createNewUser
+            return handleSuccess(res, 201, "New User crated succesfully", data)
+        } catch (error) {
+            return handleError(res, 500, error);
+        }
     }
     async login(req: Request, res: Response): Promise<Response> {
         const {error, value} = loginUserSchema.validate(req.body);
@@ -38,55 +41,93 @@ export default class UserAuth implements UserAuthInterface{
         try {
             const currentUser = await prisma.user.findUnique({
             where:{email: value.email},
-        });
-        if (!currentUser) return handleError(res, 400, "User doe not exist");
-        const verrifyPassword = await bcrypt.compare(value.password, currentUser.password);
-        if (!verrifyPassword) handleError(res, 403, "Incorrect Password");
-        
-        // create JWT
-        const accessToken: string = jwt.sign(
-        {
-            userInfo: {
-                username: currentUser.username,
-                email: currentUser.email
-            }
-        },
-        envVars.ACCESS_TOKEN_SECRET,
-        {expiresIn: "10m"}
-        );
-        const refreshToken = jwt.sign(
+            });
+            if (!currentUser) return handleError(res, 400, "User doe not exist");
+            const verrifyPassword = await bcrypt.compare(value.password, currentUser.password);
+            if (!verrifyPassword) handleError(res, 403, "Incorrect Password");
+            
+            // create JWT
+            const accessToken: string = jwt.sign(
             {
                 userInfo: {
                     username: currentUser.username,
                     email: currentUser.email
                 }
             },
-            envVars.REFRESH_TOKEN_SECRET,
-            {expiresIn: "1d"}
-        );
-        const updateUser = await prisma.user.update({
-            where: {
-                email: currentUser.email
-            },
-            data:{
-                refresh_token: refreshToken
+            envVars.ACCESS_TOKEN_SECRET,
+            {expiresIn: "10m"}
+            );
+            const refreshToken = jwt.sign(
+                {
+                    userInfo: {
+                        username: currentUser.username,
+                        email: currentUser.email
+                    }
+                },
+                envVars.REFRESH_TOKEN_SECRET,
+                {expiresIn: "1d"}
+            );
+            const updateUser = await prisma.user.update({
+                where: {
+                    email: currentUser.email
+                },
+                data:{
+                    refresh_token: refreshToken
+                }
+            });
+            console.log(refreshToken);
+            
+            res.cookie("refreshToken", refreshToken,{
+                httpOnly: true,
+                // secure: true,
+                maxAge: 24*60*60*1000
+            })
+            const data: object = {
+                access_token: accessToken,
+                id: updateUser.id,
+                user_name: updateUser.username,
+                email: updateUser.email
             }
-        });
-        res.cookie("refresh-token", refreshToken,{
-            httpOnly: true,
-            // secure: true,
-            maxAge: 24*60*60*1000
-        })
-        const data: object = {
-            access_token: accessToken,
-            id: updateUser.id,
-            user_name: updateUser.username,
-            email: updateUser.email
-        }
-        return handleSuccess(res,200, "User logged in successfuly", data);
+            return handleSuccess(res,200, "User logged in successfuly", data);
         } catch (error) {
             return handleError(res, 500, error)
         }
         
+    }
+    async logout(req: Request, res: Response): Promise<Response> {
+        const cookies = req.cookies;
+
+        if (!cookies?.refreshToken) return handleError(res, 400, "No cookies found");
+        const refreshToken = cookies.jwt;
+        try {
+            // check if user with the refresh token exists;
+            const currentUser = await prisma.user.findFirst({
+                where:{refresh_token: refreshToken}
+            });
+            if (!currentUser){
+                res.clearCookie("refreshToken", {
+                httpOnly: true,
+                  // secure: true
+                });
+                return handleError(res, 204, "User already logged out");
+            }
+            // Update the token to an empty string on the db
+            await prisma.user.update({
+                where: {
+                    id: currentUser.id
+                },
+                data: {
+                    refresh_token: ""
+                }
+            });
+            // clear the cookie
+            res.clearCookie("refreshToken", {
+            httpOnly: true,
+              // secure: true
+            });
+            return handleSuccess(res, 200, "User logged out successfully");
+        } catch (error) {
+            return handleError(res, 500, error);
+        }
     }
 }
