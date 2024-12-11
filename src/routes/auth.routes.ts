5e36cd9a8d9a56b6d96f08909ce69a27acf38ad5
Kossy-Augusta <kossyudogu@gmail.com>
2024-12-11 11:53:59 +0100

    added logout routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { Request, Response } from "express";
 import UserAuth from "../controllers/userAuthController";
+import verifyJWT from "../middleware/verifyjwt";
 const userAuth = new UserAuth
 
 const router = express.Router();
 
 router.post('/create', (req:Request, res: Response)=>{ userAuth.create(req, res)});
 router.post('/login', (req:Request, res: Response)=>{ userAuth.login(req, res)});
+router.get('/logout',verifyJWT, (req:Request, res: Response)=>{ userAuth.logout(req, res)});
 
 export default router
